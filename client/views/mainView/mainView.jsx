8990545f46bdaeb52655a3b346f7b94da725930c
James Munsch <james.a.munsch@gmail.com>
2015-06-18 20:01:43 -0500

    Update mainView.jsx

--- client/views/mainView/mainView.jsx
+++ client/views/mainView/mainView.jsx
@@ -9,21 +9,14 @@ var MainView = React.createClass({
         <ReactCSSTransitionGroup transitionName="example" transitionAppear={true}>
           <h1>JCG Capital</h1>
         </ReactCSSTransitionGroup>
-        <h3><strong>Partners:</strong> J. A. Munsch, G. H. Fitzgerald, and C. S. Harding</h3>
-<<<<<<< HEAD
         <div id="jcgCapitalApp">
         </div>
 
             <div id="rangeSlider"></div>
-            
-=======
 
         <div id="jcgCapitalChart"></div>
         <div id="jcgCapitalMenu"></div>
         <div id="jcgCapitalEditor"></div>
-
-
->>>>>>> 9c0826f8beb2bdb377042c2c04cb2fa437f94118
       </div>
     )
   }
